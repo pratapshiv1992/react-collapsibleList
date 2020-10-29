7d4e45fa662114543efeaaac826a6f39b28e0327
shiv pratap singh <pratapshiv1992@gmail.com>
2020-10-29 17:44:18 +0530

    cleanup

--- src/test/CollapsibleList.test.js
+++ src/test/CollapsibleList.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import CollapsibleList from '../components/CollapsibleList'
-import List from '../components/List'
 import {listData} from "../mockData";
 
 describe('render CollapsibleList component', () => {
--- src/test/List.test.jsx
+++ src/test/List.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-import CollapsibleList from '../components/CollapsibleList'
 import List from '../components/List'
 import {listData} from "../mockData";
 
